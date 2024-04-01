7bc91b0b3d2f7804e71a5fc76c3b6a04529875f4
Vishwajit <vishwajit.singh2215@gmail.com>
2024-04-01 18:46:20 +0530

    Completed Switch program

---
+++ Example4.java
@@ -0,0 +1,26 @@
+class Example4{
+	public static void main(String[] args){
+		int a=5;
+		int b=40;
+		int c=9;
+	
+		if(a>b){
+			if(a>c){
+				System.out.println(a+" is the greatest");	
+			}
+		
+		}
+	
+		if(b>a){
+			if(b>c){
+				System.out.println(b+" is the greatest");	
+			}
+		
+		}
+			
+		else {
+			System.out.println(c+" is the greatest");
+		}	
+		
+}
+}
---
+++ SwitchDemo.java
@@ -0,0 +1,20 @@
+class SwitchDemo{
+	public static void main(String[] args){
+		int choice =20;
+		switch(choice)
+		{
+			case 1: System.out.println("In case-1");
+				break;
+		
+			case 2: System.out.println("In case-2");
+				break;
+			
+			case 3: System.out.println("In case-3");
+				break;
+			
+			default : System.out.println("INVALID CHOICE");
+
+		}
+
+	}
+}
